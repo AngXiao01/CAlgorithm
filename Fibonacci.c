a65fd7a482ee62ef0e63aa8e36c840cb452e3880
Ang Xiao <57869791+AngXiao01@users.noreply.github.com>
2023-12-12 12:24:29 -0500

    Add files via upload

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-
-void myfunc(double num1, double num2);
-long long Fibonacci(int num3);
+long long Fibonacci(int num);
 long long arrayofFibonacci[100] = {0, 1};
 
 int main(){
-    int num3;
+    int num;
     printf("=============================================\n");
-    printf("num3:");
-    scanf("%d", &num3);
-    printf("%lld\n",Fibonacci(num3));
+    printf("num:");
+    scanf("%d", &num);
+    printf("%lld\n",Fibonacci(num));
     for (int i = 0; i < 100; i++) {
         printf("%lld ", arrayofFibonacci[i]);
     }
@@ -19,9 +17,9 @@ int main(){
 }
 
 
-long long Fibonacci(int num3){
-    if(arrayofFibonacci[num3] != 0 || num3 == 0)
-        return arrayofFibonacci[num3];
-    arrayofFibonacci[num3] = Fibonacci(num3 - 1) + Fibonacci(num3 - 2);
-    return arrayofFibonacci[num3];
+long long Fibonacci(int num){
+    if(arrayofFibonacci[num] != 0 || num == 0)
+        return arrayofFibonacci[num];
+    arrayofFibonacci[num] = Fibonacci(num - 1) + Fibonacci(num - 2);
+    return arrayofFibonacci[num];
 }
